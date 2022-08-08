8ef6caeb2503ed57754223ee615fe4253d0bd528
hjryoo <hjryoo31@gmail.com>
2022-08-08 16:35:50 +0900

    quick fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
+import type { NextPage } from "next";
 import Container from "../components/Container";
+import Image from "next/image";
+import RecentPosts from "../components/RecentPost";
+import metadata from "../data/metadata";
 import { allPosts } from "contentlayer/generated";
 import { InferGetStaticPropsType } from "next";
-import RecentPosts from "components/RecentPost";
 
 const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <Container>
       <div className={`my-5 w-full`}>
-       ...
+        <div className={`relative`}>
+          <Image
+            src={`/home.jpg`}
+            alt="main image"
+            width={`100%`}
+            height={45}
+            layout={`responsive`}
+            objectFit="cover"
+            className={`rounded-3xl`}
+          />
+          <span
+            className={`absolute top-12 font-extrabold italic text-white text-5xl md:text-9xl text flex justify-center w-full drop-shadow-lg`}
+          >
+            {metadata.title}
+          </span>
+        </div>
         <RecentPosts posts={posts} />
       </div>
     </Container>
